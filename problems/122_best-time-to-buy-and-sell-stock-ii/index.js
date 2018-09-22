18a14cf2a40ce829ad0abed1f717350ff8ab6999
10122510317 <695997058@qq.com>
2018-09-23 00:03:02 +0800

    122 simple greedy

--- problems/122_best-time-to-buy-and-sell-stock-ii/index.js
+++ problems/122_best-time-to-buy-and-sell-stock-ii/index.js
@@ -1,26 +1,23 @@
 /**
  * Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ *
  */
 /**
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-  var maxSell = minBuy = prices[0];
-  var result = 0;
-  for (var i = 1, l = prices.length; i < l; ++i) {
-    if (maxSell === undefined && prices[i] < minBuy) {
-      minBuy = prices[i];
-    } else if (prices[i] < maxSell) {
-      result += maxSell - minBuy;
-      minBuy = prices[i];
-      maxSell = undefined;
-    } else if ((maxSell === undefined && prices[i] > minBuy) || prices[i] > maxSell) {
-      maxSell = prices[i];
+const maxProfit = function(prices) {
+  let len = prices.length,
+  profit = 0,
+  dif = 0;
+  for(let i = 0; i < len-1; ++i){
+    // console.log(`prices[i]: ${prices[i]}`);
+    if((dif = prices[i+1] - prices[i]) > 0){
+        profit += dif;
+        // console.log(`profit: ${profit}`);
     }
   }
-  if (maxSell > minBuy) result += maxSell - minBuy;
-
-  return result;
-};
+  return profit;
+}
+//104ms %28.34
 module.exports = maxProfit;
--- problems/122_best-time-to-buy-and-sell-stock-ii/test-cases.js
+++ problems/122_best-time-to-buy-and-sell-stock-ii/test-cases.js
@@ -1,4 +1,14 @@
-module.exports = [{
-  input  : [[2, 1]],
-  output : 0
-}];
+module.exports = [
+  {
+    input  : [[2, 1]],
+    output : 0
+  },
+  {
+    input  : [[7,1,5,3,6,4],[1,2,3,4,5]],
+    output : 7
+  },
+  {
+    input  : [[1,2,3,4,5]],
+    output : 4
+  }
+];
