85e30869b2387b4cfec776f7a056ebe993721887
sakuraiki <695997058@qq.com>
2018-10-04 16:26:48 +0800

    73 simple array

--- problems/073_set-matrix-zeroes/index.js
+++ problems/073_set-matrix-zeroes/index.js
@@ -5,34 +5,54 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function (matrix) {
-  var m = matrix.length, n = matrix[0].length;
-  var zeroRow = false, zeroCol = false;
-  for (var i = 0; i < n; ++i)
-    if (!matrix[0][i]) zeroRow = true;
-  for (var i = 0; i < m; ++i)
-    if (!matrix[i][0]) zeroCol = true;
-  for (var i = 1; i < m; ++i) {
-    for (var j = 1; j < n; ++j) {
-      if (!matrix[i][j]) {
-        matrix[0][j] = 0;
-        matrix[i][0] = 0;
+const setZeroes = function (matrix) {
+  let m = matrix.length, n,
+    line1 = false, col1 = false;
+  if(m === 0){
+    return;
+  }else{
+    n = matrix[0].length;
+  }
+
+  for(let i = 0; i < m; ++i) {
+    if (!matrix[i][0]) {
+      col1 = true;
+    }
+  }
+  for(let i = 0; i < n; ++i) {
+    if(!matrix[0][i]){
+      line1 = true;
+    }
+  }
+
+  for(let l = 1; l < m; ++l){
+    for(let c = 1; c < n; ++c){
+      if(!matrix[l][c]){
+        matrix[l][0] = matrix[0][c] = 0;
       }
     }
   }
-  console.log(matrix);
-  for (i = 1; i < m; ++i) {
-    for (j = 1; j < n; ++j) {
-      if (!matrix[0][j] || !matrix[i][0])
-        matrix[i][j] = 0;
+
+  for(let l = 1; l < m; ++l){
+    for(let c = 1; c < n; ++c){
+      if(!matrix[l][0] || !matrix[0][c]){
+        matrix[l][c] = 0;
+      }
     }
   }
-  if (zeroRow)
-    for (var i = 0; i < n; ++i)
-      matrix[0][i] = 0;
-  if (zeroCol)
-    for (var i = 0; i < m; ++i)
-      matrix[i][0] = 0;
-};
 
+  if(col1) {
+    for (let i = 0; i < m; ++i) {
+      matrix[i][0] = 0;
+    }
+  }
+  if(line1){
+    for (let i = 0; i < n; ++i) {
+      matrix[0][i] = 0;
+    }
+  }
+
+  return;
+};
+//140ms 40.72%
 module.exports = setZeroes;
